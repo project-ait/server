ecabb32895e4c93a9d3aad8be4d0389f76a6e51d
modabada <miw1102@naver.com>
2023-10-17 12:48:30 +0900

    아이디 검증 함수 DB파트 제외하고 구현

---
+++ auth/auth.py
@@ -0,0 +1,57 @@
+import re
+
+from fastapi import APIRouter, HTTPException
+
+from auth.dto.return_code import IdValidateCode, PWValidateCode
+from auth.rule import id_rule, pw_rule
+
+router = APIRouter()
+
+
+@router.post("/register")
+async def register_user(id: str, pw: str):
+    return {"message": "개발중"}
+
+
+def validate_id(id: str) -> IdValidateCode:
+    if len(id) < id_rule.MIN_LENGTH:
+        return IdValidateCode.ID_TOO_SHORT
+    if len(id) > id_rule.MAX_LENGTH:
+        return IdValidateCode.ID_TOO_LONG
+    if is_include_not_allowed_char(id):
+        return IdValidateCode.ID_NOT_ALLOWED_CHAR
+    if len(re.findall(r"[0-9]"), id) < id_rule.MIN_NUMBER_LEN:
+        return IdValidateCode.ID_REQ_NUMBER
+    if len(re.findall(r"[A-z]"), id) < id_rule.MIN_CHAR_LEN:
+        return IdValidateCode.ID_REQ_CHAR
+    if re.search(r"(.)\1{" + str(id_rule.MAX_REPEAT_TIME) + ",}"):
+        return IdValidateCode.ID_TOO_SIMPLE
+    if has_consecutive_char(id, id_rule.MAX_REPEAT_TIME):
+        return IdValidateCode.ID_TOO_SIMPLE
+    # TODO: DB에 해당 아이디가 있는지 확인
+
+    return IdValidateCode.SUCCESS
+
+
+# 허용되지 않은 문자가 있는지만 검색
+def is_include_not_allowed_char(input_str: str) -> bool:
+    return not bool(id_rule.ALLOWED_CHAR_RE.fullmatch(input_str))
+
+
+# 1234, dcba 등 연속된 문자가 있는지 확인
+def has_consecutive_char(input_str: str, count: int) -> bool:
+    consecutive_count = 1
+    increasing = decreasing = False  # 1212 등 오르내리는 경우 처리하기 위한 변수
+    for i in range(1, len(input_str)):
+        if not decreasing and ord(input_str[i]) == ord(input_str[i - 1]) + 1:
+            consecutive_count += 1
+            increasing = True
+        elif not increasing and ord(input_str[i]) == ord(input_str[i - 1]) - 1:
+            consecutive_count += 1
+            decreasing = True
+        else:
+            consecutive_count = 1
+            increasing = decreasing = False
+        if consecutive_count > count:
+            return True
+    return False
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from auth import auth
+
+
 app = FastAPI()
 
-# app.include_router(file.router, prefix="/file", tags=["file"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
 
 @app.get("/")

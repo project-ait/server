adac3f4e93af57725cb758af9503d5674b5cd7bd
modabada <naninoo1103@gmail.com>
2023-10-18 10:20:50 +0900

    로그인 엔드포인트 구현

--- auth/auth.py
+++ auth/auth.py
@@ -20,3 +20,20 @@ def register_user(id: str, pw: str):
         jwt_token = account_util.calculate_jwt(user.jwtKey)
         return {"status": "success", "jwt": jwt_token}
     return {"status": "fail", "code": "UNKNOWN"}
+
+
+@router.post("/login")
+def login(id: str, pw: str):
+    id_val_code = account_util.validate_id(id)
+    if id_val_code != IdValidateCode.ALREADY_EXIST_ID:
+        return {"status": "fail", "code": id_val_code.name}
+    pw_val_code = account_util.validate_pw(id, pw)
+    if pw_val_code != PWValidateCode.SUCCESS:
+        return {"status": "fail", "code": pw_val_code.name}
+
+    user = sql_util.find_user(id)
+    encoded_pw = account_util.encode_pw(pw)
+    if encoded_pw != user.password:
+        return {"status": "fail", "code": "INCORRECT_PW"}
+    jwt_token = account_util.calculate_jwt(user.jwtKey)
+    return {"status": "success", "jwt": jwt_token}

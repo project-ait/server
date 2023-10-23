ee7334304b6bc19665c79cee01d0b90da18e4323
modabada <naninoo1103@gmail.com>
2023-10-23 21:07:03 +0900

    feat(weather): api impl (#15)
    * 회원탈퇴 엔드포인트 구현
    
    * JWT 헤더 작성 및 검증함수 수정
    
    * 모든 함수에 리턴타입 명시
    
    * IP를 기반으로 해당 지역의 날씨정보 불러오는 서비스 구현
    
    * 위도와 경도 선택파라미터로 변경
    
    * 1시간 단위의 날씨정보를 불러오는 api 구현
    
    * 날씨 정리
    
    * 오타 및 자료형 수정
    
    * 풍속이 0일때 부자연스러운 표현 제거
    
    * 위치정보에서 도시정보를 불러올 수 없을 때 처리
    
    ---------
    
    Co-authored-by: SEOA7777 <pleahmacaka@gmail.com>

---
+++ service/weather/dto/weather_dto.py
@@ -0,0 +1,51 @@
+import typing
+
+
+class DetailWeather:
+    def __init__(
+        self,
+        temp: float,  # 기온
+        tmin: typing.Union[float, None],  # 최저온도
+        tmax: typing.Union[float, None],  # 최고온도
+        chill: float,  # 체감온도
+        rain: int,  # 강수량 (mm/h)
+        reh: int,  # 습도 (%)
+        ws: float,  # 풍속 (m/s)
+        wd: str,  # 풍향 (한국어)
+        fdst: int,  # 미세먼지 (ug/m)
+        ffdst: int,  # 초미세먼지 (ug/m)
+    ):
+        self.temp = temp
+        self.tmin = tmin
+        self.tmax = tmax
+        self.chill = chill
+        self.rain = rain
+        self.reh = reh
+        self.ws = ws
+        self.wd = wd
+        self.fdst = fdst
+        self.ffdst = ffdst
+
+
+class ConcecutiveWeather:
+    def __init__(
+        self,
+        hour: int,  # 시 ( 0 ~ 23 )
+        stat: str,  # 날씨 (한국어)
+        temp: float,  # 기온
+        chill: float,  # 체감온도
+        rain: int,  # 강수량 (mm/h)
+        pred_rain: int,  # 강수 확률 (%)
+        ws: float,  # 풍속 (m/s)
+        wd: str,  # 풍향 (한국어)
+        reh: int,  # 습도 (%)
+    ):
+        self.hour = hour
+        self.stat = stat
+        self.temp = temp
+        self.chill = chill
+        self.rain = rain
+        self.pred_rain = pred_rain
+        self.ws = ws
+        self.wd = wd
+        self.reh = reh
---
+++ service/weather/weather.py
@@ -0,0 +1,156 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+from fastapi import APIRouter, Request
+
+from service.weather.dto.weather_dto import ConcecutiveWeather, DetailWeather
+from service.weather.weather_location_util import find_near_dong
+
+router = APIRouter()
+
+
+@router.get("/")
+def weather_detail(
+    request: Request,
+    lat: float | None = None,
+    lon: float | None = None,
+):
+    if lat == None or lon == None:
+        ip = request.client.host
+        lat, lon = get_location(ip)
+    data = find_near_dong(lat, lon)
+    if data == None:
+        return dict()
+    return get_single_weather(data.code, data.lat, data.lon)
+
+
+@router.get("/list")
+def weather_list(
+    request: Request,
+    lat: float | None = None,
+    lon: float | None = None,
+):
+    if lat == None or lon == None:
+        ip = request.client.host
+        lat, lon = get_location(ip)
+    data = find_near_dong(lat, lon)
+    if data == None:
+        return list()
+    return get_concecutive_weather(data.code, data.lat, data.lon)
+
+
+def get_location(ip: str) -> (float, float):
+    _ipinfo_url = "https://ipinfo.io"
+    _ipinfo_token = "ed776abe608c8f"
+    url = "{}/{}?token={}".format(
+        _ipinfo_url,
+        ip,
+        _ipinfo_token,
+    )
+    resp = json.loads(requests.get(url).text)
+    loc = resp["loc"].split(",")
+    return float(loc[0]), float(loc[1])
+
+
+def get_single_weather(
+    code: str,
+    lat: float,
+    lon: float,
+) -> DetailWeather:
+    body = requests.get(
+        "https://www.weather.go.kr/w/wnuri-fct2021/main/current-weather.do?&unit=m%2Fs*code={}&aws=N&lat={}&loc={}".format(
+            code,
+            lat,
+            lon,
+        )
+    ).text
+    soup = BeautifulSoup(body, "html.parser")
+
+    ul = soup.find_all("ul")
+
+    temp_section = ul[0].find("span", class_="tmp")
+    temp = float(temp_section.get_text().split("℃")[0])
+    minmax = temp_section.find_all("span")
+    tmin = minmax[2].get_text()[:-1]
+    tmin = float(tmin) if tmin.isdigit() else None
+    tmax = minmax[4].get_text()[:-1]
+    tmax = float(tmax) if tmax.isdigit() else None
+
+    # TODO: 만약 체감온도가 없으면 오류발생함
+    chill = float(ul[0].find("span", class_="chill").get_text()[3:-2])
+
+    val_section = ul[1].find_all("span", class_="val")
+    rain = val_section[2].get_text()[:-3]
+    rain = 0 if rain == "-" else float(rain)
+
+    reh = int(val_section[0].get_text()[:-1])
+
+    wind = val_section[1].get_text().split(" ")
+    ws = wind[1]
+    wd = wind[0]
+
+    dst_section = ul[3].find_all("span", class_="air-lvv")
+    fdst = float(dst_section[1].get_text())
+    ffdst = float(dst_section[0].get_text())
+
+    return DetailWeather(
+        temp=temp,
+        tmin=tmin,
+        tmax=tmax,
+        chill=chill,
+        rain=rain,
+        reh=reh,
+        ws=ws,
+        wd=wd,
+        fdst=fdst,
+        ffdst=ffdst,
+    )
+
+
+def get_concecutive_weather(
+    code: str,
+    lat: float,
+    lon: float,
+) -> list[ConcecutiveWeather]:
+    body = requests.get(
+        "https://www.weather.go.kr/w/wnuri-fct2021/main/digital-forecast.do?code={}&unit=m%2Fs&hr1=Y&lat={}&lon={}".format(
+            code,
+            lat,
+            lon,
+        ),
+    ).text
+
+    soup = BeautifulSoup(body, "html.parser")
+
+    ls = [*soup.find_all("ul", class_="vs-item"), *soup.find_all("ul", class_="s-item")]
+    ls = ls[:-24]  # 마지막 24개 (글피 데이터) 삭제
+
+    for i in range(len(ls)):
+        item = ls[i].find_all("span")
+        hour = int(item[1].get_text()[:-1])
+        stat = item[3].get_text()
+        temp = float(item[5].get_text().split("(")[0][:-1])
+        chill = float(item[8].get_text()[:-1])
+        rain = item[10].get_text()
+        rain = 0 if rain == "-" else int(rain)
+        pred_rain = item[12].get_text()[:-1]
+        pred_rain = 0 if pred_rain == "" else int(pred_rain)
+        ws = float(item[15].get_text()[:-3])
+        wd = item[14].get_text()
+        if ws == 0:  # 풍속이 0이면 '바람없음풍' 으로 출력되는 오류 수정
+            wd = wd[:-1]
+        reh = int(item[18].get_text()[:-1])
+        ls[i] = ConcecutiveWeather(
+            hour=hour,
+            stat=stat,
+            temp=temp,
+            chill=chill,
+            rain=rain,
+            pred_rain=pred_rain,
+            ws=ws,
+            wd=wd,
+            reh=reh,
+        )
+
+    return ls

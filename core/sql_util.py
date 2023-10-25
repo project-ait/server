cf580ad52af9787c6c5a2552d10ef0f9354e7cdc
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-25 14:11:18 +0900

    chore: get db host from env

--- core/sql_util.py
+++ core/sql_util.py
@@ -7,7 +7,7 @@ import psycopg2
 
 from oauth.dto.user_dto import UserDto
 
-_HOST = "localhost"
+_HOST = os.environ.get("AIT_DB_HOST")
 _PORT = 5432
 _USER = os.environ.get("AIT_DB_USER")
 _PW = os.environ.get("AIT_DB_PW")

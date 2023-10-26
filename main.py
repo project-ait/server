a5776bc99239152da686fbfc5e204a7bf8708180
modabada <naninoo1103@gmail.com>
2023-10-26 18:07:48 +0900

    지하철 도착정보 서비스 구현 및 필요없는 환경설정 파일 삭제

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 
 from core.sql_util import check_and_create_table
 from oauth import auth
+from service.subway import subway
 from service.summary import summary
 from service.weather import weather
 
@@ -14,6 +15,7 @@ app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(weather.router, prefix="/service/weather", tags=["weather"])
 app.include_router(summary.router, prefix="/service/summary", tags=["summary"])
+app.include_router(subway.router, prefix="/service/subway", tags=["subway"])
 
 check_and_create_table()
 
@@ -27,8 +29,12 @@ if __name__ == "__main__":
     uvicorn.run("main:app", port=1777, reload=True)
 
     if not os.path.exists(".env"):
-        print("ENV >> Cannot found .env file, use pre-configured environment variables.")
-        print("ENV >> If not prepared environment variables, server will be throw exception.")
+        print(
+            "ENV >> Cannot found .env file, use pre-configured environment variables."
+        )
+        print(
+            "ENV >> If not prepared environment variables, server will be throw exception."
+        )
     else:
         print("ENV >> Found .env file! load environment variables from .env file.")
         load_dotenv()

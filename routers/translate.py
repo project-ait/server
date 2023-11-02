da8e3ae8207c2d4e3719b561577cb453b83b18b6
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-02 14:33:09 +0900

    feat(translate): hard coded `/toEn` endpoint

--- routers/translate.py
+++ routers/translate.py
@@ -2,6 +2,7 @@ import os
 
 import deepl
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -9,6 +10,15 @@ API_KEY = os.environ.get("DEEPL_KEY")
 translator = deepl.Translator(API_KEY)
 
 
+class TranslatedModel(BaseModel):
+    text: str
+
+
 @router.post("/toKr")
-async def translate(text: str):
-    return translator.translate_text(text, source_lang="EN", target_lang="KO")
+async def translate(data: TranslatedModel):
+    return translator.translate_text(data.text, source_lang="EN", target_lang="KO")
+
+
+@router.post("/toEn")
+async def translate(data: TranslatedModel):
+    return translator.translate_text(data.text, source_lang="KO", target_lang="EN")

538bfc9565e61caafeea7ebbff9a5614e0fc76d2
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-02 09:26:06 +0900

    chore: shows execution

--- service/location.py
+++ service/location.py
@@ -9,8 +9,12 @@ async def get_location(address: str) -> (float, float):
     if address == "localhost" or address == "127.0.0.1":  # more specific
         address = "61.108.105.98"
 
-    details = (await handler.getDetails(address)).details
+    try:
+        details = (await handler.getDetails(address)).details
 
-    loc = details["loc"].split(",")
+        loc = details["loc"].split(",")
 
-    return float(loc[0]), float(loc[1])
+        return float(loc[0]), float(loc[1])
+    except Exception as e:
+        print(e)
+        return -1, -1
